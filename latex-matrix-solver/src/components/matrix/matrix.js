49a27b10f0c476a6016b9212befbc81cdd303100
andy-liuu <aliu3160@gmail.com>
2022-04-30 15:23:28 -0400

    documentation for matrixInput

---
+++ latex-matrix-solver/src/components/matrix/matrix.js
@@ -0,0 +1,15 @@
+import React from "react";
+function MatrixInput(props) {
+  // PROPS EXPECTED
+  // row: INT (number of rows to be rendered)
+  // col: INT (number of columns to be rendered)
+  // setMatrix: function that takes in 2d array and sets this state for matrixParent component
+
+  // RENDER EXPECTED
+  //  grid of input boxes depending on row and column prop,
+  //  submit button that calls setMatrix with whatever in inputted into the input boxes
+
+  return <div></div>;
+}
+
+export default MatrixInput;
