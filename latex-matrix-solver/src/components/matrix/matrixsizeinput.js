a1a7692b9828848d3502e14efe60116e600f21f0
andy-liuu <aliu3160@gmail.com>
2022-04-30 15:13:49 -0400

    documentation for matrixSizeInput

--- latex-matrix-solver/src/components/matrix/matrixsizeinput.js
+++ latex-matrix-solver/src/components/matrix/matrixsizeinput.js
@@ -1,6 +1,13 @@
 import React from "react";
 
 function MatrixSizeInput(props) {
+  // PROPS EXPECTED:
+  // setRow(int) function that modifies the row state in MatrixParent
+  // setCol(int) function that modifies the row state in MatrixParent
+
+  // RENDER EXPECTED:
+  // two input boxes that call setRow and setCol when the inputted number changed (and also within reasonable bounds)
+
   return <div></div>;
 }
 
