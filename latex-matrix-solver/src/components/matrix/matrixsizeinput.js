a6caead6e0d0433ac086ec035317aa3a85d90fd9
Kevin Cui <Kevincui113@gmail.com>
2022-04-30 15:26:39 -0400

    started matrixsize

--- latex-matrix-solver/src/components/matrix/matrixsizeinput.js
+++ latex-matrix-solver/src/components/matrix/matrixsizeinput.js
@@ -8,7 +8,53 @@ function MatrixSizeInput(props) {
   // RENDER EXPECTED:
   // two input boxes that call setRow and setCol when the inputted number changed (and also within reasonable bounds)
 
-  return <div></div>;
+  const form = document.getElementById("setDimensions");
+  const rowsData = form.elements["rows"];
+  const colsData = form.elements["cols"];
+
+  let currentRows = rowsData.value;
+  let currentCols = colsData.value;
+
+  return (
+
+    <form action="" id="setDimensions">
+
+      <input
+        type="number"
+        name="rows"
+        defaultValue={2}
+        onChange={e => {
+          const rows = parseInt(e.target.value)
+
+          // if we only want matrix of size between 2 and 8
+          if (1 <= rows && rows <= 8) {
+            setMatrixSize(prevSize => ({
+              ...prevSize,
+              rows: rows,
+            }))
+          }
+        }}
+      />
+
+      <input
+        type="number"
+        name="cols"
+        defaultValue={2}
+        onChange={e => {
+          const cols = parseInt(e.target.value)
+
+          // if we only want matrix of size between 2 and 8
+          if (1 <= cols && cols <= 8) {
+            setMatrixSize(prevSize => ({
+              ...prevSize,
+              cols: cols,
+            }))
+          }
+        }}
+      />
+
+    </form>
+  );
 }
 
 export default MatrixSizeInput;
