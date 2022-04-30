598e379130fae3ce607be0a8f80862c227ed4403
Kevin Cui <Kevincui113@gmail.com>
2022-04-30 14:41:03 -0400

    created converter function

---
+++ latex-matrix-solver/src/components/matrix/converter.js
@@ -0,0 +1,26 @@
+
+function converter(data) {
+    let output = "\\begin{bmatrix}";
+
+    for (let i = 0; i < data.length; ++i) {
+        for (let j = 0; j < data[0].length; ++j) {
+
+            output += " ";
+            output += `${data[i][j]}`;
+
+            if (j != data[0].length - 1) {
+                output += " &";
+            }
+
+            else {
+                if (i != data.length - 1) {
+                    output += "\\\\";
+                }
+            }
+        }
+    }
+
+    output += "\\end{bmatrix}"
+}
+
+export { converter }; 
