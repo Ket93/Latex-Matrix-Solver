eab96d33ca8270f57cb6c022d34f91f109c8f51e
Kevin Cui <Kevincui113@gmail.com>
2022-04-30 19:14:24 -0400

    fixed css

--- latex-matrix-solver/src/components/matrix/matrixout.js
+++ latex-matrix-solver/src/components/matrix/matrixout.js
@@ -1,28 +1,34 @@
 import React from "react";
+import "./matrixout.css";
 
 function MatrixOutput(props) {
 
   const grid = props.REFMatrix;
 
   return (
-    <div>
-      <table>
-        <tbody>
-          {grid.slice(0, grid.length).map((item, index) => {
-            return (
-              <tr>
-                {item.map((item2, index2) => {
-                  return (
-                    <td>{item2}</td>
-                  );
-                })
-                }
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
-      <div>Latex: {props.latexAnswer}</div>
+    <div className="displayContainer">
+      <div className="tableContainer">
+        <h2 className="outputHeading">RREF Form:</h2>
+        <table className="tableBody">
+          <tbody>
+            {grid.slice(0, grid.length).map((item, index) => {
+              return (
+                <tr>
+                  {item.map((item2, index2) => {
+                    return (
+                      <td className="tableEntry">{item2}</td>
+                    );
+                  })
+                  }
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
+      <div className="latexContainer">
+        <h2 className="latexHeading">Latex Code:</h2>
+        {props.latexAnswer}</div>
     </div>
   );
 }
