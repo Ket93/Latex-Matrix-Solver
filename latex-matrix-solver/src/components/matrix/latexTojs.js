9dfe71ef2794814379e28236aee2ddb0e0938cbf
andy-liuu <aliu3160@gmail.com>
2022-04-30 21:31:13 -0400

    int -> parseInt

--- latex-matrix-solver/src/components/matrix/latexTojs.js
+++ latex-matrix-solver/src/components/matrix/latexTojs.js
@@ -1,6 +1,7 @@
 import React from "react";
 
-const inp1 = "\begin{bmatrix} 1 & 0 & 0.423444976076555 \\ 0 & 1 & -0.043062200956937795 \\ 0 & 0 & 0 \end{bmatrix}" 
+const inp1 =
+  "\begin{bmatrix} 1 & 0 & 0.423444976076555 \\ 0 & 1 & -0.043062200956937795 \\ 0 & 0 & 0 end{bmatrix}";
 function tojs(inp) {
   console.log(inp);
 
@@ -16,19 +17,20 @@ function tojs(inp) {
         strcat += inp[i + 1];
         i += 1;
       }
-    } else { // not a valid digit
+    } else {
+      // not a valid digit
       if (inp[i] == "\\" || inp[i] == "e") {
         if (!isNaN(pusharr[0]) && pusharr[0] !== " ") {
-          pusharr.push(int(strcat.slice()));
+          pusharr.push(parseInt(strcat.slice()));
           arr.push(pusharr.slice());
           strcat = "";
           pusharr = [];
         }
-      } 
+      }
       if (inp[i] == "&") {
-        pusharr.push(int(strcat.slice()));
+        pusharr.push(parseInt(strcat.slice()));
         strcat = "";
-      } 
+      }
     }
   }
   console.log(arr);
@@ -39,24 +41,19 @@ function LatexToJs(props) {
   const handleSubmit = (event) => {
     event.preventDefault();
     console.log(event.target.str.value);
-    var val = tojs(event.target.str.value)
+    var val = tojs(event.target.str.value);
     props.setMatrix(val);
     props.setCol(val[0].length);
-    props.setRow(val.length); 
-  }
+    props.setRow(val.length);
+  };
 
   return (
     <form onSubmit={handleSubmit}>
-      <label>
-        Latex Input: 
-      </label>
-      <input
-          type="text"
-          name="str"
-        />
+      <label>Latex Input:</label>
+      <input type="text" name="str" />
       <button type="submit">Submit</button>
     </form>
-  )
+  );
 }
 
 export default LatexToJs;
